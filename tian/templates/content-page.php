38d2c2b52e6807315308a53165aef5191656b64a
romapad <romapad@gmail.com>
2016-03-23 00:33:10 +0300

    final revision

--- tian/templates/content-page.php
+++
@@ -1,9 +0,0 @@
-<section>
-  <div class="wrap container" role="document">
-    <div class="content row">  
-      <div class="col-md-12">
-<?php the_content(); ?>
-      </div>
-    </div><!-- /.content -->
-  </div><!-- /.wrap -->     
-</section>
